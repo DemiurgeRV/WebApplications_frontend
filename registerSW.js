1ef79e30a5253ba873e494bf7b91abf089668d04
Vladislav Rybin <rybinvladislav@mail.ru>
2024-03-01 19:39:48 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/WebApplications_frontend/sw.js', { scope: '/WebApplications_frontend/' })})}

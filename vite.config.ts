2ddeab466bbc68690358c20a2f8c106ac9d74c7f
Vladislav Rybin <rybinvladislav@mail.ru>
2024-03-01 20:33:44 +0300

    create pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,29 @@ export default defineConfig({
             '/api': ' http://192.168.226.36:8000/',
         }
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Photoshop',
+        short_name: 'Photoshop',
+        start_url: '/WebApplications_frontend/',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/assets/icon/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/assets/icon/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: "/WebApplications_frontend/"
 })

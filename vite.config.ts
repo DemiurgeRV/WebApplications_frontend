0342f0a3cc4fef199a07233c57cbeaab08ff8533
Vladislav Rybin <rybinvladislav@mail.ru>
2024-03-01 22:33:04 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -21,11 +21,6 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/assets/icon/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
             src: '/assets/icon/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
